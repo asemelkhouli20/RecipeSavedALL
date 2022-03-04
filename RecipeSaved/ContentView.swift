d615a0a96defd7d8a6cc79ca7a1687587ca9a9bb
asemElkhouli <asemelkhouli209@gmail.com>
2022-03-04 13:23:01 +0200

    Initial Commit

---
+++ RecipeSaved/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecipeSaved
+//
+//  Created by Asem on 04/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecipeSaved/RecipeSavedApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSavedApp.swift
+//  RecipeSaved
+//
+//  Created by Asem on 04/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct RecipeSavedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
